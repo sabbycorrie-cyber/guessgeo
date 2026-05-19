2bdbe48d67ad945816ca1dc6de123545daef19dc
OakleafOne <oliver.ekbladd@gmail.com>
2026-05-19 17:30:29 +0200

    Added the API key. Added the background image. fixed some typos in the css code. Added the background to the website.

--- guessgeo/src/Components/Game.jsx
+++ guessgeo/src/Components/Game.jsx
@@ -118,7 +118,7 @@ function Game({ user }) {
     const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
     // Build Google Street View image URL
-    const streetViewURL = `https://maps.googleapis.com/maps/api/streetview?size=600x400&location=${lat},${lon}&fov=90&heading=${heading ?? 120}&pitch=0&key=our_api_key`; // Replace 'our_api_key' with our API key //
+    const streetViewURL = `https://maps.googleapis.com/maps/api/streetview?size=600x400&location=${lat},${lon}&fov=90&heading=${heading ?? 120}&pitch=0&key=${API_KEY}`; // Replace 'our_api_key' with our API key //
 
     // Set image to display
     setImage(streetViewURL);

34fa0a24f9325e3e8faeecb91be2e057d9d8b29c
sabbycorrie-cyber <sabbycorrie@gmail.com>
2026-05-20 17:24:05 +0200

    "Added correct and wrong answer sound effects"

--- guessgeo/src/Components/Answers.jsx
+++ guessgeo/src/Components/Answers.jsx
@@ -5,9 +5,23 @@ function Answers(props) {
 const { options = [], correctAnswer, onAnswer } = props;
 const [selected, setSelected] = useState(null);
 
+const correctSound = new Audio("/sounds/correct.mp3");
+const wrongSound = new Audio("/sounds/wrong.mp3");
+
 const handleClick = (option) => {
     if (selected) return; // ignore if already selected
     setSelected(option);
+
+console.log("OPTION:", option);
+console.log("CORRECT:", correctAnswer);
+    if (option?.trim().toLowerCase() === correctAnswer?.trim().toLowerCase() 
+    ) {
+        correctSound.currentTime = 0;
+        correctSound.play();
+    } else {
+        wrongSound.currentTime = 0;
+        wrongSound.play();
+    }
 
     // Wait 1.5 seconds before reporting the answer
     setTimeout(() => {

1e244740cf20b49174ffc49a625a6aeabe839c57
tildanyrvik <tilda.nyrvik@gmail.com>
2026-05-27 16:34:04 +0200

    Debugged
    Debug

--- guessgeo/src/Components/Game.jsx
+++ guessgeo/src/Components/Game.jsx
@@ -318,7 +318,7 @@ function Game({ user }) {
     const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
     // Build Google Street View image URL
-    const streetViewURL = `https://maps.googleapis.com/maps/api/streetview?size=600x400&location=${lat},${lon}&fov=90&heading=${heading ?? 120}&pitch=0&key=
+    const streetViewURL = `https://maps.googleapis.com/maps/api/streetview?size=600x400&location=${lat},${lon}&fov=90&heading=${heading?? 120}&pitch=0&key=${API_KEY}`;
 
     // Update displayed image
     setImage(streetViewURL);

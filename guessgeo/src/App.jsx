41ea17ea8e55c5a031f6e384a7ccb1b1a3f140bd
sabbycorrie-cyber <sabbycorrie@gmail.com>
2026-06-01 15:56:35 +0200

    "Added intro screen flow before game start"

--- guessgeo/src/App.jsx
+++ guessgeo/src/App.jsx
@@ -2,14 +2,17 @@
 import { useState, useEffect } from "react";
 import Login from "./Components/Login.jsx";
 import Game from "./Components/Game.jsx";
+import IntroScreen from "./Components/IntroScreen.jsx";
 
 function App() {
 
 /* Active user state */
   const [user, setUser] = useState(null);
+  const [showIntro, setShowIntro] = useState(true);
 
 /* Restore saved user session */
 useEffect(() => {
+
   const savedUser = localStorage.getItem("currentUser");
   if (savedUser) {
     setUser(JSON.parse(savedUser));
@@ -17,16 +20,23 @@ useEffect(() => {
 }
 , []);
 
+function startGame () {
+  setShowIntro(false);
+}
+
 /* Render login or game screen */
 return (
   <div>
-    {!user ? (
-      <Login setUser={setUser} />
+    {showIntro ? (
+      <IntroScreen onStart={startGame} />
+    ) : !user ? (
+    <Login setUser={setUser} />
     ) : (
       <Game user={user} />
     )}
   </div>
 );
+
 }
 
-export default App
+export default App;

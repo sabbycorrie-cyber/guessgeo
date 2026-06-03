920c45d62e25cf372ec7f95f400b629eaad9b8da
sabbycorrie-cyber <sabbycorrie@gmail.com>
2026-06-03 16:53:35 +0200

    Secured Google API key

--- guessgeo/src/Components/Game.jsx
+++ guessgeo/src/Components/Game.jsx
@@ -315,7 +315,7 @@ function Game({ user }) {
     const { lat, lon, city, country, heading } = getRandomPlace();
 
     // Google Maps API key
-    const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
     // Build Google Street View image URL
     const streetViewURL = `https://maps.googleapis.com/maps/api/streetview?size=600x400&location=${lat},${lon}&fov=90&heading=${heading?? 120}&pitch=0&key=${API_KEY}`;

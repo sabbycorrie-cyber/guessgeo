dc4688148833641740c4f1336082b02d104d7e5d
sabbycorrie-cyber <sabbycorrie@gmail.com>
2026-06-03 16:36:37 +0200

    "Removed intro screen comments"

--- guessgeo/src/Components/IntroScreen.jsx
+++ guessgeo/src/Components/IntroScreen.jsx
@@ -7,13 +7,11 @@ return (
         <div className="intro-card">
         <h1 className="intro-title">GuessGeo</h1>
 
-        /* Game description */
         <p className="intro-subtitle">
             Explore real places from around the world.
             Study the clues, make your guess, and see how many locations you can identify.
         </p>
 
-        /* Team credits */
         <p className="credits">
             S.C.E. - Gameplay | T.N. - Locations | O.E. - Styling
         </p>
@@ -22,7 +20,6 @@ return (
             Ready to explore?
         </p>
 
-        /* Start game */
         <button className="play-button" onClick={onStart}>
             Sign In & Play
         </button>
